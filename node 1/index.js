c3085134038ac99709760e6fe9c007c2359f4620
tiwarir1 <rabi.tiwari5788@gmail.com>
2018-08-23 17:25:21 -0500

    Feat: installed express package

--- node 1/index.js
+++ node 1/index.js
@@ -1,10 +1,17 @@
-const http = require('http')
+const http = require('http')							//importing http package
 
-const fs = require('fs')
+const fs = require('fs')								//fs means file system package
+
+//const aboutPage = fs.readFileSync('about.html')				//readFileSync means it is synchronous
+																//asynchronous is blocking code, asynchronous is non blocking
+/*const aboutPage = fs.readFile('about.html', (file) =>{		//readFile is a asynchronous process
+
+})*/
+
 
 const aboutPage = fs.readFileSync('about.html')
 const contactPage = fs.readFileSync('contact.html')
-const homePage = fs.readFileSync('index.html')
+const homePage = fs.readFile('index.html')
 
 const server = http.createServer((request, response) => {
 	console.log(request.url)
