daf19758186dd009b70b48f4dec4e39e6192c274
tiwarir1 <rabi.tiwari5788@gmail.com>
2018-08-24 10:12:48 -0500

    Refactor: rewriting same code using Express framework

--- node 1/index.js
+++ node 1/index.js
@@ -1,35 +1,69 @@
-const http = require('http')							//importing http package
+// the commented section does not use any framework. it is using basic node functionalities.
+// the new code is using express framework
 
-const fs = require('fs')								//fs means file system package
+// const http = require('http')							//importing http package
 
-//const aboutPage = fs.readFileSync('about.html')				//readFileSync means it is synchronous
-																//asynchronous is blocking code, asynchronous is non blocking
-/*const aboutPage = fs.readFile('about.html', (file) =>{		//readFile is a asynchronous process
+// const fs = require('fs')								//fs means file system package
 
-})*/
+// //const aboutPage = fs.readFileSync('about.html')				//readFileSync means it is synchronous
+// 																//asynchronous is blocking code, asynchronous is non blocking
+// /*const aboutPage = fs.readFile('about.html', (file) =>{		//readFile is a asynchronous process
 
-
-const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
-const homePage = fs.readFile('index.html')
-
-const server = http.createServer((request, response) => {
-	console.log(request.url)
+// })*/
 
 
-	if (request.url === '/about'){
-		return response.end(aboutPage)
-	} else if (request.url === '/contact'){
-		return response.end(contactPage)
-	} else if (request.url === '/'){
-		return response.end(homePage)
-	} else {
-		response.writeHead(404)
-	response.end('Page not found')
-	}
+// const aboutPage = fs.readFileSync('about.html')
+// const contactPage = fs.readFileSync('contact.html')
+// const homePage = fs.readFile('index.html')
+
+// const server = http.createServer((request, response) => {
+// 	console.log(request.url)
 
 
+// 	if (request.url === '/about'){
+// 		return response.end(aboutPage)
+// 	} else if (request.url === '/contact'){
+// 		return response.end(contactPage)
+// 	} else if (request.url === '/'){
+// 		return response.end(homePage)
+// 	} else {
+// 		response.writeHead(404)
+// 	response.end('Page not found')
+// 	}
+
+
+// })
+
+// server.listen(3000)
+const path = require('path') 			//helps to get our file paths. Path change acc. to the machine the project is running on
+const express = require('express')
+
+const app = new express()
+
+// app.get('/', (request, response) => {					//get route
+// 	response.json({										//only sends json
+// 		name: 'Rabi Dada'
+// 	})
+// })	
+
+// app.get('/about', (request, response) => {
+// 	response.send({										//can send anything (file, views, templates)
+// 		name: 'Rabi Dada'
+// 	})
+// })
+
+app.get('/', (request, response) => {
+	response.sendFile(path.resolve(__dirname, 'index.html')) 		//from module called path. __dirname gets current directory. syntax is path.resolve(__dirname, 'filename') 
 })
 
-server.listen(3000)
+app.get('/about', (request, response) =>{
+	response.sendFile(path.resolve(__dirname,'about.html'))
+})
 
+app.get('/contact', (request, response) => {
+	response.sendFile(path.resolve(__dirname, 'contact.html'))
+})
+
+app.listen(3000, () => {
+	console.log('App listening on 3000')
+})
