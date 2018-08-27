58f56b9c1c16f709f8f42a1a23558e06c9fe749e
tiwarir1 <rabi.tiwari5788@gmail.com>
2018-08-27 09:49:55 -0500

    Refactor: using templating engiene and layouts instead of duplicating common elements like footer and navbar)

--- node-blog/index.js
+++ node-blog/index.js
@@ -1,25 +1,28 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
+const expressEdge = require('express-edge')
 
 const app = new express()
 
 app.use(express.static('public'))
+app.use(expressEdge)
+app.set('views', `${__dirname}/views`)
 
 app.get('/', (request, response) => {
-	response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+	response.render('index')
 })
 
 app.get('/about', (request, response) => {
-	response.sendFile(path.resolve(__dirname, 'pages/about.html'))
+	response.render('about')
 })
 
 app.get('/post', (request, response) =>{
-	response.sendFile(path.resolve(__dirname, 'pages/post.html'))
+	response.render('post')
 })
 
 app.get('/contact', (request, response) => {
-	response.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+	response.render('contact')
 })
 
 app.listen(4000, () => {
